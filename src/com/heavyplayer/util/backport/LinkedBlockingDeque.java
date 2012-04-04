ee7f4277ff38e4d3e6b63321ad23a0a10c903bc2
Gonçalo Silva <goncalossilva@gmail.com>
2012-04-04 22:56:16 +0100

    Change package.

--- src/com/heavyplayer/util/backport/LinkedBlockingDeque.java
+++ src/com/heavyplayer/util/backport/LinkedBlockingDeque.java
@@ -5,7 +5,7 @@
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
  
-package javabackports.util.concurrent;
+package com.heavyplayer.util.backport;
 
 import java.util.AbstractQueue;
 import java.util.Collection;
